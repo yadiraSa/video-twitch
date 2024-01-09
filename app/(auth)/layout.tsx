ab78adda09f4f5a6bc2e4dfdbe18060213b6339e
yadiraSa <yadira.salazar@clickittech.com.mx>
2024-01-09 12:56:57 -0600

    add: :sparkles: authentication

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,5 @@
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div className="h-full flex items- justify-center">{children}</div>;
+};
+
+export default AuthLayout;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <Button size="lg" variant='outline'>
-      Click Me
-    </Button>
+    <div className='flex flex-col gap-y-4'>
+      <h1>Dashboard</h1>
+      <UserButton afterSignOutUrl="/" />
+    </div>
   );
 }
